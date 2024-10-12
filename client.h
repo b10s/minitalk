db29a670970c23302ad206c2a10aa1ae6d9509d5
Enshin Andrei <b1os@bk.ru>
2024-10-12 21:20:15 +0900

    norm

--- client.h
+++ client.h
@@ -4,12 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void send_byte(int pid, unsigned char b);
-void ack_handler();
-void err_handler();
-void err(char *msg);
-void send_msg(char *msg, int pid);
-size_t ft_strlen(const char *s);
-char * ft_itoa(int n);
-int ft_atoi(const char *str);
-
+void	send_byte(int pid, unsigned char b);
+void	ack_handler(void);
+void	err_handler(void);
+void	err(char *msg);
+void	send_msg(char *msg, int pid);
+size_t	ft_strlen(const char *s);
+char	*ft_itoa(int n);
+int		ft_atoi(const char *str);
